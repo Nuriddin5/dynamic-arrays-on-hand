a09b98bf9c18875d0cfe5bec5c8f087c080774c7
Nuriddin <nuriddinqurbonboyev@mail.ru>
2022-02-15 21:09:55 +0500

    commit before removeAt

--- src/com/nuriddin/Main.java
+++ src/com/nuriddin/Main.java
@@ -19,10 +19,12 @@ public class Main {
         array.insert(25);
         array.insert(25);
         array.insert(25);
-        array.insert(25);
+        array.insert(22);
         array.insert(25);
         System.out.println(array.indexOfLastElement());
         System.out.println(Arrays.toString(array.arr));
+        System.out.println(array.indexOf(22));
+        System.out.println(array.indexOf(0));
 
     }
 }
