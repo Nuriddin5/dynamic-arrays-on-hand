ea53f1cba53ecd2b27c4344e46381d5954080a7a
Nuriddin <nuriddinqurbonboyev@mail.ru>
2022-02-26 22:02:26 +0500

    optimized and changed structure

--- src/com/nuriddin/Array.java
+++ src/com/nuriddin/Array.java
@@ -4,47 +4,46 @@ package com.nuriddin;
 // t.me/superJavaDeveloper 15.02.2022;
 
 
-import java.util.Arrays;
-
 public class Array {
-    int length;
-    int[] arr;
+    private int count;
+    private int[] arr;
 
-    public Array(int length) {
-        this.length = length;
-        this.arr = new int[length];
+    public Array(int count) {
+        this.arr = new int[count];
+    }
+
+    public int[] getArr() {
+        return arr;
     }
 
 
-    public void insert(int number) {
-        if ((indexOfLastElement() == arr.length - 1)) {
-            int[] newArr = new int[++length];
-            for (int i = 0; i < arr.length; i++) {
+    public int getCount() {
+        return count;
+    }
+
+
+    public void print() {
+        System.out.println("items : ");
+        for (int i = 0; i < count; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("length : " + arr.length);
+    }
+
+
+    public void insert(int item) {
+        if (arr.length > count) {
+            arr[count++] = item;
+        } else {
+            int[] newArr = new int[arr.length * 2];
+            for (int i = 0; i < count; i++) {
                 newArr[i] = arr[i];
             }
-            newArr[length-1] = number;
+            newArr[count++] = item;
             arr = newArr;
-        } else {
-            arr[indexOfLastElement() + 1] = number;
         }
     }
 
-    public int indexOfLastElement() {
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (arr[i] != 0) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public boolean removeAt(int index) {
-        int[] newArr = new int[--length];
-
-        
-        return true;
-
-    }
 
     public int indexOf(int number) {
         for (int i = 0; i < arr.length; i++) {
@@ -53,5 +52,33 @@ public class Array {
             }
         }
         return -1;
+    }
+
+    public void removeAt(int index) {
+        //validate index
+        //if last element remove last
+        //else shift elements beginning index
+        if (index < 0 || index >= count) {
+            throw new IllegalArgumentException("ee insofsiz yaxshilab ishlat");
+        }
+        if (count - 1 == arr.length / 2) {
+            int[] newArr = new int[arr.length / 2];
+            //[1,2,3,4,0,0] index -> 1
+            // [1,3,4]
+            for (int i = index; i < count; i++) {
+                arr[i] = arr[i + 1];
+            }
+            count--;
+            for (int i = 0; i < count; i++) {
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+        } else {
+            for (int i = index; i < count - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[count - 1] = 0;
+            count--;
+        }
     }
 }
